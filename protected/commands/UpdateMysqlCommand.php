167e757a05fe049ae56279a8cdb5ed50f1390e88
info@magnusbilling.com <info@magnusbilling.com>
2018-01-23 14:42:44 -0300

    adicionando cores para as categorias

--- protected/commands/UpdateMysqlCommand.php
+++ protected/commands/UpdateMysqlCommand.php
@@ -62,6 +62,68 @@ class UpdateMysqlCommand extends ConsoleCommand
             $this->executeDB($sql);
         }
 
+        if ($version == '3.0.4') {
+
+            $sql = "UPDATE `pkg_configuration` SET `config_description` = '1 to active, 0 to inactive ' WHERE config_key = 'amd';
+
+            UPDATE `pkg_configuration` SET `status` = '0';
+            UPDATE `pkg_configuration` SET `status` = '1' WHERE config_key = 'base_language';
+            UPDATE `pkg_configuration` SET `status` = '1' WHERE config_key = 'version';
+            UPDATE `pkg_configuration` SET `status` = '1' WHERE config_key = 'admin_email';
+            UPDATE `pkg_configuration` SET `status` = '1' WHERE config_key = 'portabilidadeUsername';
+            UPDATE `pkg_configuration` SET `status` = '1' WHERE config_key = 'portabilidadePassword';
+            UPDATE `pkg_configuration` SET `status` = '1' WHERE config_key = 'operator_next_try';
+            UPDATE `pkg_configuration` SET `status` = '1' WHERE config_key = 'updateAll';
+            UPDATE `pkg_configuration` SET `status` = '1' WHERE config_key = 'campaign_limit';
+            UPDATE `pkg_configuration` SET `status` = '1' WHERE config_key = 'tardanza';
+            UPDATE `pkg_configuration` SET `status` = '1' WHERE config_key = 'valor_colectivo';
+            UPDATE `pkg_configuration` SET `status` = '1' WHERE config_key = 'valor_hora_zero';
+            UPDATE `pkg_configuration` SET `status` = '1' WHERE config_key = 'valor_hora';
+            UPDATE `pkg_configuration` SET `status` = '1' WHERE config_key = 'valor_falta';
+            UPDATE `pkg_configuration` SET `status` = '1' WHERE config_key = 'notify_url_after_save_number';
+            UPDATE `pkg_configuration` SET `status` = '1' WHERE config_key = 'notify_url_category';
+            UPDATE `pkg_configuration` SET `status` = '1' WHERE config_key = 'record_call';
+            UPDATE `pkg_configuration` SET `status` = '1' WHERE config_key = 'dialcommand_param';
+            UPDATE `pkg_configuration` SET `status` = '1' WHERE config_key = 'MixMonitor_format';
+            ALTER TABLE `pkg_category` ADD `color` VARCHAR(7) NOT NULL DEFAULT '#ffffff' AFTER `use_in_efetiva`;
+
+
+
+            UPDATE `pkg_category` SET `color` = '#FF0000' WHERE id = 0;
+            UPDATE `pkg_category` SET `color` = '#339966' WHERE id = 1;
+            UPDATE `pkg_category` SET `color` = '#ddb96d' WHERE id = 2;
+            UPDATE `pkg_category` SET `color` = '#FF99CC' WHERE id = 3;
+            UPDATE `pkg_category` SET `color` = '#ab6b40' WHERE id = 4;
+            UPDATE `pkg_category` SET `color` = '#800080' WHERE id = 5;
+            UPDATE `pkg_category` SET `color` = '#00FF00' WHERE id = 6;
+            UPDATE `pkg_category` SET `color` = '#d9d1a8' WHERE id = 7;
+            UPDATE `pkg_category` SET `color` = '#8d5ed5' WHERE id = 8;
+            UPDATE `pkg_category` SET `color` = '#993366' WHERE id = 9;
+            UPDATE `pkg_category` SET `color` = '#FF0000' WHERE id = 10;
+            UPDATE `pkg_category` SET `color` = '#99CCFF' WHERE id = 11;
+
+
+            ";
+            $this->executeDB($sql);
+
+            $sql = "DELETE FROM pkg_category WHERE name = 'Inactivo' AND status = 0;";
+            $this->executeDB($sql);
+
+            $sql = "INSERT INTO `pkg_category` VALUES (99,'Inativo','',0,0);
+            UPDATE `pkg_category` SET `id` = '0' WHERE `id` = 99;";
+            $this->executeDB($sql);
+
+            $sql = "ALTER TABLE  `pkg_category` ADD  `type` TINYINT( 1 ) NOT NULL DEFAULT  '1';";
+            $this->executeDB($sql);
+
+            $sql = "UPDATE  `pkg_category` SET  `type` =  '0' WHERE  `pkg_category`.`id` =0;";
+            $this->executeDB($sql);
+
+            $version = '3.0.5';
+            $sql     = "UPDATE pkg_configuration SET config_value = '" . $version . "' WHERE config_key = 'version' ";
+            $this->executeDB($sql);
+        }
+
     }
 
     private function executeDB($sql)
--- protected/models/Category.php
+++ protected/models/Category.php
@@ -1,58 +1,57 @@
 <?php
 /**
  * Modelo para a tabela "Campaign".
- * MagnusSolution.com <info@magnussolution.com> 
+ * MagnusSolution.com <info@magnussolution.com>
  * 28/10/2012
  */
 
 class Category extends Model
 {
-	protected $_module = 'category';
-	/**
-	 * Retorna a classe estatica da model.
-	 * @return Campaign classe estatica da model.
-	 */
-	public static function model($className = __CLASS__)
-	{
-		return parent::model($className);
-	}
+    protected $_module = 'category';
+    /**
+     * Retorna a classe estatica da model.
+     * @return Campaign classe estatica da model.
+     */
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
 
-	/**
-	 * @return nome da tabela.
-	 */
-	public function tableName()
-	{
-		return 'pkg_category';
-	}
+    /**
+     * @return nome da tabela.
+     */
+    public function tableName()
+    {
+        return 'pkg_category';
+    }
 
-	/**
-	 * @return nome da(s) chave(s) primaria(s).
-	 */
-	public function primaryKey()
-	{
-		return 'id';
-	}
+    /**
+     * @return nome da(s) chave(s) primaria(s).
+     */
+    public function primaryKey()
+    {
+        return 'id';
+    }
 
-	/**
-	 * @return array validacao dos campos da model.
-	 */
-	public function rules()
-	{
-		return array(
-			array('name', 'required'),
-			array('status,', 'numerical', 'integerOnly'=>true),
-			array('description', 'length', 'max'=>100),
-			array('type', 'length', 'max'=>15)
-		);
-	}
+    /**
+     * @return array validacao dos campos da model.
+     */
+    public function rules()
+    {
+        return array(
+            array('name', 'required'),
+            array('status,', 'numerical', 'integerOnly' => true),
+            array('description', 'length', 'max' => 100),
+            array('type,color', 'length', 'max' => 15),
+        );
+    }
 
-
-	public function beforeSave()
-	{
-		if (strlen($this->type) > 0) {
-			unset($this->status);
-			unset($this->type);
-		}
-		return parent::beforeSave();
-	}
+    public function beforeSave()
+    {
+        if (strlen($this->type) > 0) {
+            unset($this->status);
+            unset($this->type);
+        }
+        return parent::beforeSave();
+    }
 }
